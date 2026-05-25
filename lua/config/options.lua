e395664313bd7c77f43f8d108eb49564efecdd35
t1mursadykov <i@t1mr.ru>
2026-05-25 12:53:14 +0300

    chore: telescope command

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -13,7 +13,7 @@ vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 
 vim.opt.updatetime = 250
-vim.opt.cmdheight = 0
+vim.opt.cmdheight = 1
 vim.opt.laststatus = 3
 vim.opt.cursorline = true
 vim.opt.ignorecase = true
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -2,15 +2,22 @@ local M = {}
 
 function M.register()
     require("which-key").add({
+        { "<leader>f<cr>", "<cmd>Telescope resume<cr>", desc = "Resume last search" },
         { "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find files" },
         { "<leader>fg", "<cmd>Telescope live_grep<cr>", desc = "Live grep" },
         { "<leader>fb", "<cmd>Telescope buffers<cr>", desc = "Buffers" },
-        { "<leader>f<cr>", "<cmd>Telescope resume<cr>", desc = "Resume last search" },
+        { "<leader>fo", "<cmd>Telescope oldfiles<cr>", desc = "Recent files" },
         { "<leader>fm", "<cmd>Telescope bookmarks list<cr>", desc = "Bookmarks" },
         { "<leader>fs", "<cmd>Telescope lsp_document_symbols<cr>", desc = "Document symbols" },
         { "<leader>fS", "<cmd>Telescope lsp_workspace_symbols<cr>", desc = "Workspace symbols" },
         { "<leader>fr", "<cmd>Telescope lsp_references<cr>", desc = "LSP references" },
+        { "<leader>fh", "<cmd>Telescope help_tags<cr>", desc = "Help tags" },
+        { "<leader>fk", "<cmd>Telescope keymaps<cr>", desc = "Keymaps" },
+        { "<leader>fc", "<cmd>Telescope commands<cr>", desc = "Commands" },
+        { "<leader>fd", "<cmd>Telescope diagnostics<cr>", desc = "Diagnostics" },
+        { "<leader>fw", "<cmd>Telescope grep_string<cr>", desc = "Grep word under cursor" },
         { "<leader>gs", "<cmd>Telescope git_status<cr>", desc = "Git status" },
+        { "<leader>gc", "<cmd>Telescope git_commits<cr>", desc = "Git commits" },
     })
 end
 
