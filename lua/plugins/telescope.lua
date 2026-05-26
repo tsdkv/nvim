f6b11c1e6a74136d98a022db6ed64ed4be2fd290
t1mursadykov <i@t1mr.ru>
2026-05-27 00:09:30 +0300

    feat(telescope): make buffer command

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -36,11 +36,57 @@ function M.setup()
     ts.load_extension("bookmarks")
     ts.load_extension("fzf")
 
+    local function cool_buffers()
+        local opts = {
+            previewer = false,
+            sort_mru = false,
+            sort_lastused = false,
+            show_bufnr = false,
+            ignore_current_buffer = true,
+            prompt_title = false,
+            results_title = false,
+            layout_config = {
+                width = 0.55,
+                height = 0.35,
+            },
+            initial_mode = "insert",
+        }
+
+        opts.attach_mappings = function(prompt_bufnr, map)
+            local action_state = require("telescope.actions.state")
+
+            map("n", "d", actions.delete_buffer)
+            map("i", "<C-d>", actions.delete_buffer)
+
+            -- Automatically open the buffer if it is the only match remaining
+            vim.api.nvim_buf_attach(prompt_bufnr, false, {
+                on_lines = function()
+                    vim.schedule(function()
+                        if not vim.api.nvim_buf_is_valid(prompt_bufnr) then
+                            return
+                        end
+                        local picker = action_state.get_current_picker(prompt_bufnr)
+                        if picker then
+                            local prompt = picker:_get_prompt()
+                            if #prompt > 0 and picker.manager:num_results() == 1 then
+                                actions.select_default(prompt_bufnr)
+                            end
+                        end
+                    end)
+                end,
+            })
+
+            return true
+        end
+
+        require("telescope.builtin").buffers(require("telescope.themes").get_dropdown(opts))
+    end
+
     require("which-key").add({
         { "<leader>f<cr>", "<cmd>Telescope resume<cr>", desc = "Resume last search" },
         { "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find files" },
         { "<leader>fg", "<cmd>Telescope live_grep<cr>", desc = "Live grep" },
-        { "<leader>fb", "<cmd>Telescope buffers<cr>", desc = "Buffers" },
+        { "<leader>fb", cool_buffers, desc = "Buffers" },
         { "<leader>fo", "<cmd>Telescope oldfiles<cr>", desc = "Recent files" },
         { "<leader>fm", "<cmd>Telescope bookmarks list<cr>", desc = "Bookmarks" },
         { "<leader>fs", "<cmd>Telescope lsp_document_symbols<cr>", desc = "Document symbols" },
