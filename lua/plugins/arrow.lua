128f4f4034c7731b10b5f275a6a8c88021dd141a
t1mursadykov <i@t1mr.ru>
2026-05-27 00:09:09 +0300

    fix(arrow): window style

--- lua/plugins/arrow.lua
+++ lua/plugins/arrow.lua
@@ -7,6 +7,24 @@ M.setup = function()
         show_icons = true,
         leader_key = ";",
         buffer_leader_key = "m",
+
+        window = {
+            width = "auto",
+            height = "auto",
+            row = "auto",
+            col = "auto",
+            border = "rounded",
+        },
+    })
+
+    -- Force the Arrow floating window background to match the main editor background
+    vim.api.nvim_create_autocmd("BufWinEnter", {
+        group = vim.api.nvim_create_augroup("ArrowWindowStyle", { clear = true }),
+        callback = function(ev)
+            if vim.b[ev.buf].filename and vim.b[ev.buf].arrow_current_mode then
+                vim.wo.winhighlight = "Normal:Normal,FloatBorder:Comment"
+            end
+        end,
     })
 end
 
