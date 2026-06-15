243e0d98d5042a6e654eae5a206a84121949c60c
t1mursadykov <i@t1mr.ru>
2026-06-15 22:14:13 +0300

    fix: change flash keymap to enter

--- lua/plugins/flash.lua
+++ lua/plugins/flash.lua
@@ -1,11 +1,46 @@
 local M = {}
 
 M.keymap = {
-    { "s", function() require("flash").jump() end, mode = { "n", "x", "o" }, desc = "Flash Jump" },
-    { "gS", function() require("flash").treesitter() end, mode = { "n", "x", "o" }, desc = "Flash Treesitter" },
-    { "r", function() require("flash").remote() end, mode = "o", desc = "Remote Flash" },
-    { "R", function() require("flash").treesitter_search() end, mode = { "o", "x" }, desc = "Treesitter Search" },
-    { "<c-s>", function() require("flash").toggle() end, mode = "c", desc = "Toggle Flash Search" },
+    {
+        "<CR>",
+        function()
+            require("flash").jump()
+        end,
+        mode = { "n", "x", "o" },
+        desc = "Flash Jump",
+    },
+    {
+        "S",
+        function()
+            require("flash").treesitter()
+        end,
+        mode = { "n", "x", "o" },
+        desc = "Flash Treesitter",
+    },
+    {
+        "r",
+        function()
+            require("flash").remote()
+        end,
+        mode = "o",
+        desc = "Remote Flash",
+    },
+    {
+        "R",
+        function()
+            require("flash").treesitter_search()
+        end,
+        mode = { "o", "x" },
+        desc = "Treesitter Search",
+    },
+    {
+        "<c-s>",
+        function()
+            require("flash").toggle()
+        end,
+        mode = "c",
+        desc = "Toggle Flash Search",
+    },
 }
 
 M.setup = function()
